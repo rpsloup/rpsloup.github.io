78d034ac1e0f0ea08d60ba89f10342b977766c9e
rpsloup <robin.patrik.sloup@proton.me>
2022-11-12 12:26:30 +0100

    Creating the Default Layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import DefaultLayout from '../layouts/DefaultLayout';
+
 const HomePage = () => {
   return (
-    <>
+    <DefaultLayout>
       <h1>Robin Patrik Sloup</h1>
       <p>Coming soon...</p>
-    </>
+    </DefaultLayout>
   );
 }
 
