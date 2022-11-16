1e8472f562b7a817b3b994e4e4a2bebfff43d378
rpsloup <robin.patrik.sloup@proton.me>
2022-11-16 08:24:31 +0100

    Creating the Icon element

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import DefaultLayout from '../layouts/DefaultLayout';
+import Icon from '../components/elements/Icon';
 
 const HomePage = () => {
   return (
     <DefaultLayout>
       <h1>Robin Patrik Sloup</h1>
+      <Icon name="child" type="fas" />
       <p>Coming soon...</p>
     </DefaultLayout>
   );
