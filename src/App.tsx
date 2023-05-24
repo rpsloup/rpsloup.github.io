0262d7f8c4ff1c9e65e600a9b3fcd5e3f568aae6
rpsloup <rpsloup32@proton.me>
2023-05-24 10:39:56 +0200

    Adding the Terminus font

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Desktop from './components/Desktop';
 import Taskbar from './components/Taskbar/Taskbar.component';
 
 import './styles/reset.scss';
+import './styles/fonts.scss';
+import './styles/main.scss';
 
 const App = (): JSX.Element => (
   <Desktop>
