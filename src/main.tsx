30decbd9fb18cfc9078c89f01124bdea72ca7805
rpsloup <rpsloup32@proton.me>
2023-05-24 09:40:55 +0200

    Adding and setting up Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import App from './App.tsx';
 
+import store from './redux/store.ts';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>
 );
